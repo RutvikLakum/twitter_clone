c23fa886e905fde96f368576fd8d0725400a5e89
luckrut <rutviklakum003@gmail.com>
2023-05-09 11:27:25 +0530

    first commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "6450dc7e71a8cc49b853";
+  static const String projectId = "6450d8f98274a2848659";
+  static const String endpoint = "http://localhost:80/v1";
+}
---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/apis/auth_api.dart';
+import 'package:twitter_clone/core/utils.dart';
+
+
+final authcontrollerProvider = StateNotifierProvider<AuthController,bool>((ref) {
+  return  AuthController(authAPI: ref.watch(authAPIProvider));
+});
+
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authAPI;
+  AuthController({required AuthAPI authAPI}) :_authAPI = authAPI, super(false);
+
+  void signUP ({
+required String email,
+required String password,
+required BuildContext context,
+}) async {
+    state = true;
+    final res = await _authAPI.signUp(email: email, password: password);
+   
+res.fold((l) => showSnackBar(context, l.message), 
+(r) => print(r.email));
+
+  }} 
+
+
+  
